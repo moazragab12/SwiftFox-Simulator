02bd3d5451e3c9d4e4120b0d67f85cc959f5c354
Moaz Ragab <moazr8365@gmail.com>
2025-04-09 23:27:46 +0200

    Change container type of gantt chart only

--- cpu_scheduler/src/main/java/com/example/SecondaryController.java
+++ cpu_scheduler/src/main/java/com/example/SecondaryController.java
@@ -2,9 +2,12 @@ package com.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 
 public class SecondaryController {
 
+    @FXML
+     private VBox chartContainer;
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
